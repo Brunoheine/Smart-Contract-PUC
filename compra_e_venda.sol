9d686eeb388682167b7f846881a8efaa581886b2
Brunoheine <92693019+Brunoheine@users.noreply.github.com>
2021-10-19 22:16:27 -0300

    1º Alteração - 19/10/2021
    Incrementei mais funções (Construct e Return)

--- compra_e_venda.sol
+++ compra_e_venda.sol
@@ -2,8 +2,8 @@ pragma solidity 0.8.9;
 
 contract Compra_e_venda {
  
-  string comprador;
-  string vendedor;
+  string comprador; //nome do comprador
+  string vendedor; //nome do vendedor
   string matricula;//matricula do imóvel
   string cartorio; //local da matricula do imóvel
   string DataVencimento;
@@ -13,14 +13,36 @@ contract Compra_e_venda {
   uint ValorEmAberto;
   uint ValorDaParcela;
   uint QuantidadeDeParcelas; //compra a prazo
-  uint PorcentagemDaMulta;
+  uint public PorcentagemDaMulta = 10;
  
+  constructor
+    (uint _valorTotal, uint _quantidadeDeParcelas, uint _ValorEntrada, string memory _matricula, string memory _cartorio)
+ 
+ {    ValorTotal = _valorTotal;
+      QuantidadeDeParcelas = _quantidadeDeParcelas;
+      ValorDaEntrada = _ValorEntrada;
+      matricula = _matricula;
+      cartorio = _cartorio;
+  }
+  
   function PagarEntrada(uint ValorPagamento) public returns(string memory, uint){
       ValorDaEntrada = ValorPagamento;
       ValorEmAberto = ValorTotal - ValorPagamento;
       return("O seu valor em aberto de", ValorEmAberto);
   
   }
- 
+   function PagarParcela(uint ValorPagamento) public returns(string memory, uint){
+      ValorDaParcela = ValorPagamento;
+      ValorEmAberto = ValorEmAberto - ValorDaParcela;
+      return("Valor em aberto:", ValorEmAberto);
+   } 
+   
+   function ValorParcela () public view returns (string memory, uint){
+       return ("Valor da Parcela:", (ValorTotal - ValorDaEntrada)/QuantidadeDeParcelas);
+   }
+   
+   function ValorMulta () public view returns (string memory, uint){
+       return ("Valor da Multa:", (ValorTotal)*PorcentagemDaMulta/100);
+   }
 }    
 
