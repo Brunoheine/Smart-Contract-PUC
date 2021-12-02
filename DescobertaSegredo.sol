959db6d5cbd580d7fa8d7f29be396998126088cc
Brunoheine <92693019+Brunoheine@users.noreply.github.com>
2021-12-02 14:26:06 -0300

    1º tentativa - aula 25/11

---
+++ DescobertaSegredo.sol
@@ -0,0 +1,18 @@
+pragma solidity 0.8.9;
+
+interface ISecret {
+    function segredo() external view returns (string memory);
+}
+
+contract DescobertaSegredo {
+
+    ISecret public DescobertaSegredo;
+
+    DescobertaSegredo = ISecret(0x6092c93dD90D05940b8DC2DCbec728152A5c245F);
+    
+    DescobertaSegredo.
+    
+    function versegredo () public view returns (string memory)
+        { return }
+
+}
